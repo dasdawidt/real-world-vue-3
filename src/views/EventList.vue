38354406f2da18d96fb88db51300fdaedeab1703
David Maier <david.maier@schmalz.de>
2022-09-22 09:08:15 +0000

    format

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="events">
-    <EventCard v-for="event in events" :key="event.id" :event="event" />
-  </div>
+    <div class="events">
+        <EventCard v-for="event in events" :key="event.id" :event="event" />
+    </div>
 </template>
 
 <script>
@@ -9,31 +9,31 @@ import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
 
 export default {
-  name: 'EventList',
-  components: {
-    EventCard
-  },
-  data() {
-    return {
-      events: null
-    }
-  },
-  created() {
-    EventService.getEvents()
-      .then(response => {
-        this.events = response.data
-      })
-      .catch(error => {
-        console.log(error)
-      })
-  }
+    name: 'EventList',
+    components: {
+        EventCard,
+    },
+    data() {
+        return {
+            events: null,
+        }
+    },
+    created() {
+        EventService.getEvents()
+            .then((response) => {
+                this.events = response.data
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    },
 }
 </script>
 
 <style scoped>
 .events {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 }
 </style>
