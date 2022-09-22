2c4f5d7c3029dde8577a6ca4c2118fbed9f66eaf
David Maier <david.maier@schmalz.de>
2022-09-22 08:31:13 +0000

    set up L4-own

--- src/router/index.js
+++ src/router/index.js
@@ -3,21 +3,21 @@ import EventList from '@/views/EventList.vue'
 import About from '@/views/About.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'EventList',
-    component: EventList
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: About
-  }
+    {
+        path: '/',
+        name: 'EventList',
+        component: EventList,
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About,
+    },
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
 })
 
 export default router
